cb0cb53403b55872096cea8a4f67d1ab855d8d12
yaohunzhanyue <yaohunzhanyue@gmail.com>
2018-09-03 22:09:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
 cd /mt
+docker run --ulimit nofile=90000:90000 -d -p443:443 --name=mtproto-proxy --restart=always -v proxy-config:/data -e SECRET=1dbf5c48562888c34c4dab4a97c9e780 telegrammessenger/proxy:latest
+docker logs mtproto-proxy
